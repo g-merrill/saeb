058a2aa27c93bb0b5941225e8e26adfa7e6419e7
Greg <gmerrill03@gmail.com>
2019-12-29 15:45:51 -0800

    Configure test environment

--- src/pages/App.test.js
+++ src/pages/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders variety of smoups link', () => {
+  const { getByText } = render(<Router><App /></Router>);
+  const linkElement = getByText(/variety of smoups/i);
   expect(linkElement).toBeInTheDocument();
 });
