41d7c260155509bf2b61a26f24a350f0c6c0601d
Greg <gmerrill03@gmail.com>
2019-12-29 19:42:38 -0800

    Add logo render test for App

--- src/pages/App.test.js
+++ src/pages/App.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 import App from './App';
 
-test('renders variety of smoups link', () => {
-  const { getByText } = render(<Router><App /></Router>);
-  const linkElement = getByText(/variety of smoups/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders variety of smoups link', ()=> {
+    const renderedApp = render(<Router><App /></Router>);
+    const linkElement = renderedApp.getByText('variety of smoups');
+    expect(linkElement).toBeInTheDocument();
+  });
+  it('renders logo', async () => {
+    const renderedApp = render(<Router><App /></Router>);
+    const logoElement = renderedApp.getByAltText('saeb logo');
+    expect(logoElement).toBeInTheDocument();
+  });
 });
