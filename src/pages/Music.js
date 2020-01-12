36771a4b2943df6a1b2285d0d822ad740ce85047
Greg <gmerrill03@gmail.com>
2020-01-11 20:09:23 -0800

    Add Music content

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './css/Music.scss';
 
 const Music = () => (
-  <div className="Music">Music</div>
+  <div className="content Music">
+    <h1 className="section-title">Music</h1>
+    <p className="main-text">
+      We always sing together at our Sunday Assemblies. It’s a great way to have fun, bring up the energy, and feel part of a positive group!
+    </p>
+    <p className="main-text">
+      To help the music along, a handful of people from our community lead the singing, and maybe even play an instrument or two (or four). This small group varies in personnel, and you can get involved, too. Just get in touch, and we’ll point you in the right direction!
+    </p>
+    <p className="main-text">
+      The “band” usually chooses the songs for the month, but if you’d like to suggest a favorite tune, let us know, and we’ll pass the idea on to them.
+    </p>
+  </div>
 );
 
 export default Music;
