163cd9d452b3bfe8985c2b523757d70466eb4c31
Greg <gmerrill03@gmail.com>
2020-01-19 09:07:57 -0800

    Add additional page content

--- src/pages/PastSpeakers.js
+++ src/pages/PastSpeakers.js
@@ -2,7 +2,57 @@ import React from 'react';
 import './css/PastSpeakers.scss';
 
 const PastSpeakers = () => (
-  <div className="PastSpeakers">PastSpeakers</div>
+  <div className="content PastSpeakers">
+    <h1 className="section-title">Past Speakers</h1>
+    <p className="main-text">
+      Every month, at our main event—the Sunday Assembly celebration—we have a guest speaker. Their topic might make us Wonder More, or tell us how we can Help Often, or Live Better.
+    </p>
+    <p className="main-text">
+      Over the years, we’ve heard many wonderful and fascinating speakers.
+    </p>
+    <p className="main-text">
+      Some examples of past speakers:
+    </p>
+    <h4 className="main-subtitle">
+      Margaret Zhao
+    </h4>
+    <p className="main-text">
+      Our speaker this  month was Margaret Zhao. She was introduced by her publicist, who read several humorous haiku and got us laughing. Margaret then gave an engrossing talk about her difficulties growing up under the communists in China, which make up the story she tells in her memoir Really Enough.
+    </p>
+    <p className="main-text">
+      Also a standup comedian, Ms. Zhao mixed in jokes and told how her mother and she used humor to get through all the hard times before she got to the U.S.
+    </p>
+    <h4 className="main-subtitle">
+      Richard Lewis
+    </h4>
+    <p className="main-text">
+      Richard Lewis, the Executive Director of Save the Bay, gave a very grounded talk about appreciating the wonderful, nature-filled area we live in. He’s led Save the Bay for twenty years and filled us in on its history, showing beautiful photos of the bay, the birds (his favorite being pelicans!), and the people who help maintain it for the enjoyment of everyone.
+    </p>
+    <h4 className="main-subtitle">
+      Ruby Ming
+    </h4>
+    <p className="main-text">
+      Our guest speaker, Ruby Ming, spoke about how art affects the brain. She described how art activates different parts of the brain that using language or logic. By engaging in the arts, we develop connections between those different parts as artistic endeavors activate all these sides.
+    </p>
+    <p className="main-text">
+      Art also helps us increase cognitive function, decrease stress, and people who engage in arts report better moods. This does not need to be high-art, or even producing anything worthy of an audience. You can get all these benefits through any creative expression. Ruby then described artists, art making cognitive loads and what one might need to make art: Time, space, and tools.
+    </p>
+    <h4 className="main-subtitle">
+      Robin Ball
+    </h4>
+    <p className="main-text">
+      Our guest speaker this month was Dr. Robin Ball, a lecturer in neuroscience at the Molecular and Cell Biology Department at UC Berkeley. Dr. Ball laid down the foundations of how neurons in the brain work, and then discussed three projects funded by the BRAIN initiative.
+    </p>
+    <p className="main-text">
+      It was cool hearing how new discoveries are pointing toward a future where paraplegics will be able to send nerve impulses from their brain through a device to their prosthetics or their own muscles. She did a wonderful job of explaining complex concepts clearly with a very energetic style!
+    </p>
+    <h4 className="main-subtitle">
+      Jane Fischberg
+    </h4>
+    <p className="main-text">
+      We had Jane Fischberg for our speaker on Sunday. The president of Rubicon Programs, Jane informed us of Rubicon’s mission to break the cycle of poverty in the East Bay. She described how they have gone beyond job placement and developed a wider approach to assisting people in finding lasting success. Jane was eager to hear and answer our questions, and had help from two of her associates from Rubicon who also attended the celebration.
+    </p>
+  </div>
 );
 
 export default PastSpeakers;
--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -2,7 +2,30 @@ import React from 'react';
 import './css/Resources.scss';
 
 const Resources = () => (
-  <div className="Resources">Resources</div>
+  <div className="content Resources">
+    <h1 className="section-title">Resources</h1>
+    <h2 className="section-title">Leadership Resources</h2>
+    <p className="main-subtitle">Board Resources</p>
+    <p className="main-text">
+      <a className='main-link' href='https://drive.google.com/open?id=0B778_lEr-7YWfng1dm9NNWQzc3N0WUJTbEt3dU5POTlBR0ZOUFNHWkJwbnUwQ0lLTTFrNUk' target='_blank' rel="noopener noreferrer">Board Minutes</a>: In the hopes of transparency, leadership discussions and minutes are made public after every meeting.
+    </p>
+    <p className="main-subtitle">Financial Information</p>
+    <p className="main-text">
+      <a className='main-link' href='https://docs.google.com/spreadsheets/d/1gNRXrsLjIyRHqd9fZuMbQ7BaISsJKzFmKlz2UkAjIDA/edit?usp=sharing' target='_blank' rel="noopener noreferrer">Latest Budget and Balance Sheet</a>: Here we account for income, expense both expected and actual
+    </p>
+    <p className="main-subtitle">Other Policies:</p>
+    <p className="main-text">
+      <a className='main-link' href='http://eastbay.sundayassembly.com/wp-content/uploads/31/2015/02/SAEB-Code-of-Conduct-15-5-25.pdf' target='_blank' rel="noopener noreferrer">SAEB Code of Conduct 15-5-25</a>
+      <a className='main-link' href='http://eastbay.sundayassembly.com/wp-content/uploads/31/2015/02/2015-03-10_SAEB-Bylaws.pdf' target='_blank' rel="noopener noreferrer">2015-03-10_SAEB Bylaws</a>
+    </p>
+    <h2 className="section-title">Participant Resources</h2>
+    <p className="main-subtitle">Guides:</p>
+    <p className="main-text">
+      <p className="main-text">How to Start a Smoup (coming soon)</p>
+      <p className="main-text">How to Write a “Trying My Best” (coming soon)</p>
+      <a className='main-link' href='https://docs.google.com/document/d/1umZz3fR-IqvLu8axEPKP4ogSJhhwJq68z7t26xnMBc8/export?format=pdf' target='_blank' rel="noopener noreferrer">Speaker Guidelines</a>
+    </p>
+  </div>
 );
 
 export default Resources;
