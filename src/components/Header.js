481d9e31f538eb729b6256491470eac82c378bcc
Greg <gmerrill03@gmail.com>
2020-01-19 08:26:05 -0800

    Begin adding new color theme to site

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,21 @@ import './css/Header.scss';
 
 const Header = () => {
   const history = useHistory();
-  const handleHeaderClick = () => {
+  const handleLogoClick = () => {
     history.push('/saeb');
   };
   return (
-    <header onClick={handleHeaderClick} className='Header' >
+    <header className='Header' >
       <img 
         className='logo' 
-        src='https://i.imgur.com/77VuBTQ.png' 
-        alt='saeb logo' 
+        src='https://i.imgur.com/Fc1iS9H.png' 
+        alt='saeb logo'
+        onClick={handleLogoClick}
       />
-      <h1 className='site-title'>Sunday Assembly East Bay</h1>
-      <h4 className='site-subtitle'>A Secular Community</h4>
+      <div className='titles-ctnr'>
+        <h1 className='site-title'>Sunday Assembly East Bay</h1>
+        <h4 className='site-subtitle'>A Secular Community</h4>
+      </div>
     </header>
 )};
 
